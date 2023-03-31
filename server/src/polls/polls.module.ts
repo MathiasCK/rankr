35d439ce29a34e6ccfd1ec096fb507ea313766a9
mathiasck <mathias.ck2000@gmail.com>
2023-03-31 11:11:06 +0200

    Create redis connection

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,10 +1,11 @@
+import { redisModule } from '@/modules.config';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from '@polls';
 import { PollsService } from '@polls/polls.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, redisModule],
   controllers: [PollsController],
   providers: [PollsService],
 })
