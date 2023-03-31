eb482a37f1ac1da85946fc86fc9f2b3b1ce6641f
mathiasck <mathias.ck2000@gmail.com>
2023-03-31 09:58:51 +0200

    Add path alias for polls

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PollsModule, PollsController } from './polls';
+import { PollsModule, PollsController } from '@polls';
 
 @Module({
   imports: [ConfigModule.forRoot(), PollsModule],
