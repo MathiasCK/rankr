a77b61344e465b0f32021b0b5ab880d248e98e21
mathiasck <mathias.ck2000@gmail.com>
2023-05-11 15:49:49 +0200

    Set up routes for pages & manage app state

---
+++ client/src/pages/Join.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join: React.FC = () => {
+  return (
+    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sum">
+      <h3 className="text-center">Enter code provided by a Friend</h3>
+    </div>
+  );
+};
+
+export default Join;
--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { AppPage, actions } from '../state';
 
 const Welcome: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full">
+    <header className="flex flex-col justify-center items-center h-full">
       <h1 className="text-center my-12">Welcome to Rankr</h1>
-    </div>
+      <section className="my-12 flex flex-col justify-center">
+        <button
+          className="box btn-orange my-2"
+          onClick={() => actions.setPage(AppPage.Create)}
+        >
+          Create new poll
+        </button>
+        <button
+          className="box btn-purple my-2"
+          onClick={() => actions.setPage(AppPage.Join)}
+        >
+          Join existing poll
+        </button>
+      </section>
+    </header>
   );
 };
 
