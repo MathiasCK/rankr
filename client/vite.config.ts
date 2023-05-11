16487b83df2a57dab004e09675b4f17e472dd08c
mathiasck <mathias.ck2000@gmail.com>
2023-05-11 15:33:06 +0200

    Create path aliases for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import path from 'node:path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@stories': path.resolve(__dirname, './src/stories'),
+      '@components': path.resolve(__dirname, './src/components/index'),
+      '@components/*': path.resolve(__dirname, './src/components/*'),
+      '@pages': path.resolve(__dirname, './src/pages/index'),
+      '@pages/*': path.resolve(__dirname, './src/pages/*'),
+    },
+  },
   plugins: [react()],
   server: {
     port: 8080,
