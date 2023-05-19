a5f1db8c63d9f41dd9e1dfab91a5a394f9d2cd76
mathiasck <mathias.ck2000@gmail.com>
2023-05-19 13:53:30 +0200

    Add UI for joining poll

--- client/src/pages/Join.tsx
+++ client/src/pages/Join.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { actions } from '../state';
 
 const Join: React.FC = () => {
+  const [name, setName] = useState('');
+  const [pollID, setPollID] = useState('');
+  const [apiError, setApiError] = useState('');
+
+  const validateFields = (): boolean => {
+    return pollID.length === 6 && name.length > 0 && name.length <= 25;
+  };
+
+  const handleJoinPoll = () => console.log('Join');
+
   return (
-    <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sum">
-      <h3 className="text-center">Enter code provided by a Friend</h3>
-    </div>
+    <section className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sum">
+      <article className="mb-12">
+        <div className="my-4">
+          <h3 className="text-center">
+            Enter code provided by &quot;Friend&quot;
+          </h3>
+          <div className="text-center w-full">
+            <input
+              className="box info w-full"
+              maxLength={6}
+              onChange={(e) => setPollID(e.target.value)}
+              autoCapitalize="characters"
+              style={{ textTransform: 'uppercase' }}
+            />
+          </div>
+        </div>
+        <div className="my-4">
+          <div className="text-center">Your name</div>
+          <div className="text-center w-full">
+            <input
+              className="box info w-full"
+              maxLength={25}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+        </div>
+        {apiError && (
+          <p className="text-center text-red-600 font-light mt-8">{apiError}</p>
+        )}
+      </article>
+      <div className="my-12 flex flex-col justify-center items-center">
+        <button
+          className="box btn-orange w-32 my-2"
+          disabled={!validateFields()}
+          onClick={handleJoinPoll}
+        >
+          Join
+        </button>
+        <button
+          className="box btn-purple w-32 my-2"
+          onClick={() => actions.startOver()}
+        >
+          Start Over
+        </button>
+      </div>
+    </section>
   );
 };
 
