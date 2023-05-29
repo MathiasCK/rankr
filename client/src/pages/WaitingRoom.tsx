d20cacf3cd6488aa4902a25b2ba9d6b6b3d0b9f2
mathiasck <mathias.ck2000@gmail.com>
2023-05-29 17:41:00 +0200

    Update WaitingRoom component

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
-import { actions } from '../state';
+import React, { useEffect, useState } from 'react';
+import { actions, state } from '../state';
+import { useSnapshot } from 'valtio';
+import ConfirmationDialog from '@components/ui/ConfirmationDialog';
+import ParticipantList from '@components/ParticipantList';
+import NominationForm from '@components/NominationForm';
+import { Actions, Header, Footer } from '@components/WaitingRoom';
 
 const WaitingRoom: React.FC = () => {
   useEffect(() => {
     actions.initializeSocket();
   }, []);
   return (
-    <div className="flex flex-col w-full justify-between items-center h-full">
-      <h3>Waiting Room</h3>
-    </div>
+    <>
+      <section className="flex flex-col w-full justify-between items-center h-full">
+        <Header />
+        <Actions />
+        <Footer />
+      </section>
+    </>
   );
 };
 
