7224d47533736668c49829e272c5a880e9ddda0e
mathiasck <mathias.ck2000@gmail.com>
2023-05-29 17:40:47 +0200

    Update WaitingRoom Actions component

--- client/src/components/WaitingRoom/Actions.tsx
+++ client/src/components/WaitingRoom/Actions.tsx
@@ -1,36 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdPeopleOutline } from 'react-icons/md';
 import { useSnapshot } from 'valtio';
-import { state } from '../../state';
+import { actions, state } from '../../state';
 import { BsPencilSquare } from 'react-icons/bs';
+import NominationForm from '../NominationForm';
+import ParticipantList from '../ParticipantList';
+import ConfirmationDialog from '../ui/ConfirmationDialog';
 
-interface Props {
-  setIsParticipantListOpen: (value: boolean) => void;
-  setIsNominationFormOpen: (value: boolean) => void;
-}
-
-const Actions: React.FC<Props> = ({
-  setIsParticipantListOpen,
-  setIsNominationFormOpen,
-}) => {
+const Actions = () => {
   const snap = useSnapshot(state);
+
+  const [isNominationFormOpen, setIsNominationFormOpen] = useState(false);
+  const [isParticipantListOpen, setIsParticipantListOpen] = useState(false);
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+  const [participantToRemove, setParticipantToRemove] = useState<string>();
+
+  const confirmRemoveParticipant = (id: string) => {
+    setConfirmationMessage(`Remove ${snap.poll?.participants[id]} from poll?`);
+    setParticipantToRemove(id);
+    setIsConfirmationOpen(true);
+  };
+
+  const submitRemoveParticipant = () => {
+    participantToRemove && actions.removeParticipant(participantToRemove);
+    setIsConfirmationOpen(false);
+  };
   return (
-    <article className="flex justify-center">
-      <button
-        className="box btn-orange mx-2 pulsate"
-        onClick={() => setIsParticipantListOpen(true)}
-      >
-        <MdPeopleOutline size={24} />
-        <span>{snap.participantCount}</span>
-      </button>
-      <button
-        className="box btn-purple mx-2 pulsate"
-        onClick={() => setIsNominationFormOpen(true)}
-      >
-        <BsPencilSquare size={24} />
-        <span>{snap.nominationCount}</span>
-      </button>
-    </article>
+    <>
+      <article className="flex justify-center">
+        <button
+          className="box btn-orange mx-2 pulsate"
+          onClick={() => setIsParticipantListOpen(true)}
+        >
+          <MdPeopleOutline size={24} />
+          <span>{snap.participantCount}</span>
+        </button>
+        <button
+          className="box btn-purple mx-2 pulsate"
+          onClick={() => setIsNominationFormOpen(true)}
+        >
+          <BsPencilSquare size={24} />
+          <span>{snap.nominationCount}</span>
+        </button>
+      </article>
+      <NominationForm
+        title={snap.poll?.topic}
+        isOpen={isNominationFormOpen}
+        onClose={() => setIsNominationFormOpen(false)}
+        onSubmitNomination={(nominationText: string) =>
+          actions.nominate(nominationText)
+        }
+        nominations={snap.poll?.nominations}
+        userID={snap.me?.id}
+        onRemoveNomination={(nominationID: string) =>
+          actions.removeNomination(nominationID)
+        }
+        isAdmin={snap.isAdmin || false}
+      />
+      <ParticipantList
+        isOpen={isParticipantListOpen}
+        onClose={() => setIsParticipantListOpen(false)}
+        participants={snap.poll?.participants}
+        onRemoveParticipant={confirmRemoveParticipant}
+        isAdmin={snap.isAdmin || false}
+        userID={snap.me?.id}
+      />
+      <ConfirmationDialog
+        showDialog={isConfirmationOpen}
+        message={confirmationMessage}
+        onConfirm={() => submitRemoveParticipant()}
+        onCancel={() => setIsConfirmationOpen(false)}
+      />
+    </>
   );
 };
 
