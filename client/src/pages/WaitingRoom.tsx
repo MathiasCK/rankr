9c831d9ce7745f47c9c32a788036a5d20ffe693c
mathiasck <mathias.ck2000@gmail.com>
2023-05-29 14:56:46 +0200

    Fire socket.io connection from waitingRoom

--- client/src/pages/WaitingRoom.tsx
+++ client/src/pages/WaitingRoom.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { actions } from '../state';
 
 const WaitingRoom: React.FC = () => {
+  useEffect(() => {
+    actions.initializeSocket();
+  }, []);
   return (
     <div className="flex flex-col w-full justify-between items-center h-full">
       <h3>Waiting Room</h3>
